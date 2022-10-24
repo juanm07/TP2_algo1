47fdebc9d553a05106cdc317399b217ff22d66c9
juanm07 <63691303+juanm07@users.noreply.github.com>
2022-10-24 15:58:38 -0300

    Update minasAdyacentesTEST.cpp

--- TESTS/minasAdyacentesTEST.cpp
+++ TESTS/minasAdyacentesTEST.cpp
@@ -7,3 +7,36 @@
 #include "gtest/gtest.h"
 
 using namespace std;
+
+TEST(minasAdyacentesTEST, esquina_1){
+    tablero t = {
+            {cMINA,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cMINA},
+            {cVACIA,cMINA,cVACIA}
+    };
+    pos p = {1,0};
+    int res = minasAdyacentes(t,p);
+    ASSERT_EQ(res, 2);
+}
+
+TEST(minasAdyacentesTEST, esquina_2){
+    tablero t = {
+            {cMINA,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cMINA},
+            {cVACIA,cMINA,cVACIA}
+    };
+    pos p = {0,0};
+    int res = minasAdyacentes(t,p);
+    ASSERT_EQ(res, 0);
+}
+
+TEST(minasAdyacentesTEST, medio){
+    tablero t = {
+            {cMINA,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cMINA},
+            {cVACIA,cMINA,cVACIA}
+    };
+    pos p = {1,1};
+    int res = minasAdyacentes(t,p);
+    ASSERT_EQ(res, 3);
+}
