05f1e879c84dbfd97d44aec1ab3e45c8150dfcce
juanm07 <63691303+juanm07@users.noreply.github.com>
2022-10-24 16:55:35 -0300

    agrego Tests

--- TESTS/perdioTEST.cpp
+++ TESTS/perdioTEST.cpp
@@ -6,3 +6,33 @@
 #include "gtest/gtest.h"
 
 using namespace std;
+
+TEST(perdioTEST, YaPerdio){
+    tablero t = {
+            {cVACIA,cVACIA,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cMINA,cVACIA},
+            {cVACIA,cMINA,cVACIA,cMINA},
+            {cVACIA,cMINA,cVACIA,cMINA}
+    };
+    jugadas j = {
+            jugada(pos(0,0),0),jugada(pos(0,1),1), jugada(pos(0,2),1),
+            jugada(pos(1,0),1), jugada(pos(1,2),2)
+    };
+    bool res = perdio(t,j);
+    ASSERT_TRUE(res);
+}
+
+TEST(perdioTEST, noPerdioAun){
+    tablero t = {
+            {cVACIA,cVACIA,cVACIA,cVACIA},
+            {cVACIA,cVACIA,cMINA,cVACIA},
+            {cVACIA,cMINA,cVACIA,cMINA},
+            {cVACIA,cMINA,cVACIA,cMINA}
+    };
+    jugadas j = {
+            jugada(pos(0,0),0),jugada(pos(0,1),1), jugada(pos(0,2),1),
+            jugada(pos(1,0),1), jugada(pos(1,3),2), jugada(pos(3,0),2)
+    };
+    bool res = perdio(t,j);
+    ASSERT_FALSE(res);
+}
