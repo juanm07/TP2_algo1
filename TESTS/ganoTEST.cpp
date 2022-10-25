a7988cb1f36fe788e89681c74e53e876f451a7dd
viaguirre <93285792+viaguirre@users.noreply.github.com>
2022-10-25 11:12:25 -0300

    agrego test siGano

--- TESTS/ganoTEST.cpp
+++ TESTS/ganoTEST.cpp
@@ -38,3 +38,16 @@ TEST(ganoTEST, todaviaNoGano){
     jugadas j = jugadasValidas;
     ASSERT_FALSE(gano(t, j));
 }
+
+
+/******++++**************************** NUESTRO TEST ***********+++***********************/
+TEST(ganoTEST, siGano){
+    jugadas j = {
+            jugada(pos(0, 2), 2),jugada(pos(0, 4), 1), jugada(pos(0,3), 1),
+            jugada(pos(1, 0), 3),jugada(pos(1, 1), 3),jugada(pos(1, 2), 3),jugada(pos(1, 4), 1),
+            jugada(pos(2, 0), 1),jugada(pos(2, 2), 2),jugada(pos(2, 3), 1),jugada(pos(2, 4), 1),
+            jugada(pos(3, 0), 2),jugada(pos(3, 1), 3),jugada(pos(3, 2), 2),jugada(pos(3, 3), 1),jugada(pos(3, 4), 0),
+            jugada(pos(4, 1), 2),jugada(pos(4, 3), 1),jugada(pos(4, 4), 0)
+    };
+    ASSERT_TRUE(gano(t, j));
+}
