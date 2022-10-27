0e95ed504f4ae7cdbb635ef86299162cebba1d7a
juanm07 <63691303+juanm07@users.noreply.github.com>
2022-10-27 16:37:46 -0300

    complejidad (revisar) y jugar plus

--- Ejercicios/auxiliares.cpp
+++ Ejercicios/auxiliares.cpp
@@ -1,196 +1,83 @@
 //
-// Este archivo contiene las definiciones de las funciones auxiliares
-// que ustedes necesiten implementar. Tienen que coincidir con las 
-// declaraciones de auxiliares.h
+// Este archivo contiene las definiciones de las funciones que implementan los ejercicios
 //
 
+#include <stdio.h>
 #include <iostream>
+
 
 #include "definiciones.h"
 #include "ejercicios.h"
 #include "auxiliares.h"
+
 using namespace std;
 
 /******++++**************************** EJERCICIO minasAdyacentes ***********+++***********************/
-bool esAdyacenteValida(pos p, tablero& t){
-    int largo_tablero = t.size();
-    if(coordenadaValida(p.first,largo_tablero) && coordenadaValida(p.second,largo_tablero)){
-        return true;
-    }
-    return false;
-}
 
-bool coordenadaValida(int c, int n){
-    return (0<= c && c<n);
-}
-
-
-
-
-/******++++**************************** EJERCICIO plantarBanderita ***********+++***********************/
-
-bool perteneceABanderitas(pos p, banderitas& b){
-    for(int i=0; i<b.size();i++){
-        if(b[i]== p){
-            return true;
-        }
-    }
-    return false;
-}
-
-void removerBanderita(pos p, banderitas& b){
-    for(int i = 0; i<b.size();i++){
-        if(b[i]==p){
-            intercambiarValoresVector(b[i],b[b.size()-1]);
-            b.pop_back();
-        }
-    }
-}
-
-void intercambiarValoresVector(pos& x, pos& y){
-    pos temp = x;
-    x = y;
-    y = temp;
-}
-
-/******++++**************************** EJERCICIO perdio ***********+++***********************/
-
-bool seJugoUnaMina(tablero& t, jugadas& j){
-    for(int i = 0; i<j.size();i++){
-        pos posicionJugada = j[i].first;
-        if(t[posicionJugada.first][posicionJugada.second]){
-            return true;
-        }
-    }
-    return false;
-}
-
-/******++++**************************** EJERCICIO jugarPlus ***********+++***********************/
-
-bool existeCaminoValido(tablero& t,banderitas& b,pos posJugada,jugadas& j){
-    int minas_ady_a_posJugada = minasAdyacentes(t,posJugada);
-    if(noEsMina(t,posJugada) && minas_ady_a_posJugada == 0 && hayCaminoLibre(t,b,posJugada,j)){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-bool hayCaminoLibre(tablero& t, banderitas& b, pos posInicial, jugadas& j){
-    if(existeAdyacenteValida(t,b,posInicial,j)){
-        return true;
-    }
-    return false;
-}
-
-bool existeAdyacenteValida(tablero& t, banderitas& b, pos p, jugadas& j){
+int minasAdyacentes(tablero& t, pos p) {
+    //Complejidad: O(1) constante
+    int res = 0;
     for(int i = -1; i<2;i++){
-        for(int k = -1;k<2;k++){
-            pos adyacente = {(p.first)-i, (p.second)-k};
-            if(esCasillaDeCamino(t,b,adyacente,j) || esFinalDeCamino(t,b,adyacente,j)){
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-bool noEsMina(tablero& t, pos p){
-    if(t[p.first][p.second]){
-        return false;
-    }
-    else{
-        return true;
-    }
-}
-
-bool esCasillaDeCamino(tablero& t, banderitas& b, pos p, jugadas& j){
-    return (esAdyacenteValida(p,t) && minasAdyacentes(t,p)== 0 && noEsMina(t,p)
-            && !perteneceABanderitas(p,b) && !posEstaEnJugadas(p,j));
-}
-
-bool posEstaEnJugadas(pos p, jugadas& j){
-    for(int i = 0;i<j.size();i++){
-        if(j[i].first == p){
-            return true;
-        }
-    }
-    return false;
-}
-
-
-void caminoLibre(tablero& t, banderitas& b, pos p, jugadas& j) {
-        if (esCasillaDeCamino(t, b, p, j)) {
-            j.push_back(jugada(pos(p), minasAdyacentes(t,p)));
-            for (int i = -1; i < 2; i++) {
-                for (int k = -1; k < 2; k++) {
-                    pos posNueva = {p.first - i, p.second - k};
-                    if (esCasillaDeCamino(t, b, posNueva, j)) {
-                        j.push_back(jugada(pos(posNueva), minasAdyacentes(t, posNueva)));
-                    }
-                    else {
-                        if(esFinalDeCamino(t, b, posNueva, j)){
-                            j.push_back(jugada(pos(posNueva), minasAdyacentes(t, posNueva)));
-                        }
-                    }
-                    posNueva = p;
-                }
-            }
-            caminoLibre(t,b,p,j);
-       }
-        else {
-            j.push_back(jugada(pos(p), minasAdyacentes(t, p)));
-        }
-}
-bool esFinalDeCamino(tablero& t, banderitas& b, pos p, jugadas& j){
-    if(minasAdyacentes(t,p)>=1 && noEsMina(t,p) && !posEstaEnJugadas(p,j) &&
-       !perteneceABanderitas(p,b)){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-bool esAdyacenteAcamino(tablero& t, banderitas& b, pos p, jugadas& j){
-    for(int i = -1; i<2;i++){
-        for(int k = -1; k<2;k++){
-            pos posAdescubrir = {p.first -i , p.second -k};
-            if(esCasillaDeCamino(t,b,posAdescubrir,j)){
-                return true;
-            }
-        }
-    }
-    return false;
-}
-/******++++**************************** EJERCICIO gano ***********+++***********************/
-bool juegoGanado(vector<pos> p, jugadas& j){
-    for(int i = 0; i < p.size(); i++){
-        if(!perteneceAJugadas(p[i], j)){
-            return false;
-        }
-    }
-    return true;
-}
-
-bool perteneceAJugadas(pos p, jugadas& j){
-    for(int i = 0; i < j.size(); i++){
-        if(j[i].first == p){
-            return true;
-        }
-    }
-    return false;
-}
-
-vector<pos> posicionesSinMinas (tablero& t){
-    vector<pos> res = { };
-    for(int i = 0; i < t.size(); i++) {
-        for (int k = 0; k < t.size(); k++) {
-            if(!t[i][k]){
-                res.push_back(make_pair(i,k));
+        for(int j = -1;j<2;j++){
+            pos posicion_actual = {(p.first)- i,(p.second)-j};
+            if(esAdyacenteValida(posicion_actual,t)&& t[posicion_actual.first][posicion_actual.second] && (i != 0 || j!=0)){
+                res++;
             }
         }
     }
     return res;
+}
+
+/******++++**************************** EJERCICIO plantarBanderita ***********+++***********************/
+
+void cambiarBanderita(tablero& t, jugadas& j, pos p, banderitas& b) {
+    //Complejidad: O(n)
+    int res = 0;
+    for(int i=0; i<b.size();i++){
+        if(b[i]== p){
+            intercambiarValoresVector(b[i],b[b.size()-1]);
+            b.pop_back();
+            res++;
+        }
+    }
+    if(res==0){
+        b.push_back(p);
+    }
+}
+
+/******++++**************************** EJERCICIO perdio ***********+++***********************/
+bool perdio(tablero& t, jugadas& j) {
+    //Complejidad: O(n)
+    if(seJugoUnaMina(t,j)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+/******++++**************************** EJERCICIO gano ***********+++***********************/
+bool gano(tablero& t, jugadas& j) {
+    //Complejidad O(n^5) ?
+    return juegoGanado(posicionesSinMinas(t), j);
+}
+
+/******++++**************************** EJERCICIO jugarPlus ***********+++***********************/
+void jugarPlus(tablero& t, banderitas& b, pos p, jugadas& j) {
+    //Complejidad: O(n^8)
+    if(casillaValida(p,t,b,j)){
+        descubreAutomatico(p,t,b,j);
+    }else{
+        j.push_back(jugada(p, minasAdyacentes(t, p)));
+    }
+}
+/******++++**************************** EJERCICIO sugerirAutomatico121 ***********+++***********************/
+
+bool sugerirAutomatico121(tablero& t, banderitas& b, jugadas& j, pos& p) {
+    //Complejidad: O(n^3)
+    if(!perteneceAJugadas(p, j) && !perteneceABanderitas(p, b) && esAdyacente121(p, j)){
+        return true;
+    }
+    return false;
 }
 
