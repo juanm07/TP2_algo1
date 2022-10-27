3c6b71c33c3d97f5de5ff3a02f8850d1b330650c
juanm07 <63691303+juanm07@users.noreply.github.com>
2022-10-27 17:00:39 -0300

    Update auxiliares.h

--- Ejercicios/auxiliares.h
+++ Ejercicios/auxiliares.h
@@ -12,9 +12,6 @@
 /******++++**************************** EJERCICIO plantarBanderita ***********+++***********************/
 bool perteneceABanderitas(pos p, banderitas& b);
 void intercambiarValoresVector(pos& x, pos& y);
-void removerBanderita(pos p, banderitas& b);
-
-
 /******++++**************************** EJERCICIO minasAdyacentes ***********+++***********************/
 bool esAdyacenteValida(pos p, tablero& t);
 bool coordenadaValida(int c, int n);
@@ -22,12 +19,20 @@ bool coordenadaValida(int c, int n);
 /******++++**************************** EJERCICIO perdio ***********+++***********************/
 bool seJugoUnaMina(tablero& t, jugadas& j);
 
+/******++++**************************** EJERCICIO gano ***********+++***********************/
+bool juegoGanado(vector<pair<int,int>> p,jugadas& j);
+bool perteneceAJugadas(pos p, jugadas& j);
+vector<pair<int,int>> posicionesSinMinas (tablero& t);
+
 /******++++**************************** EJERCICIO jugarPlus ***********+++***********************/
-bool existeCaminoValido(tablero& t,banderitas& b,pos posJugada,jugadas& j);
-bool hayCaminoLibre(tablero& t, banderitas& b, pos posInicial, jugadas& j);
-bool existeAdyacenteValida(tablero& t, banderitas& b, pos p, jugadas& j);
-bool noEsMina(tablero& t, pos p);
-bool esCasillaDeCamino(tablero& t, banderitas& b, pos p, jugadas& j);
-bool posEstaEnJugadas(pos p, jugadas& j);
-jugadas caminoLibre(tablero& t, banderitas& b, pos p, jugadas& j);
-bool esFinalDeCamino(tablero& t, banderitas& b, pos p, jugadas& j);
+vector<pos> posicionesFinalesDeCamino(pos p,tablero& t,banderitas& b,jugadas& j);
+void finalDeCamino(tablero& t, banderitas& b, pos p, jugadas& j);
+vector<pos> posicionesAdyacentes(pos p,tablero& t,banderitas& b,jugadas& j);
+bool casillaValida(pos p, tablero& t, banderitas& b, jugadas& j);
+bool esUnaMina(pos p, tablero& t);
+void descubreAutomatico(pos p, tablero& t, banderitas& b, jugadas& j);
+/******++++**************************** EJERCICIO sugerirAutomatico121 ***********+++***********************/
+bool esAdyacente121(pos p, jugadas& j);
+bool es121Horizontal(pos p, jugadas& j);
+bool es121Vertical(pos p, jugadas& j);
+bool perteneceJugada(jugada j0, jugadas& j);
