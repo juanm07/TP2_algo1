dc52f45a873ec5ad8aefe5a26f2581b1195c585d
juanm07 <63691303+juanm07@users.noreply.github.com>
2022-12-04 23:14:56 -0300

    Update ejercicios.h

--- Ejercicios/ejercicios.h
+++ Ejercicios/ejercicios.h
@@ -8,9 +8,9 @@
 #include "definiciones.h"
 
 int minasAdyacentes(tablero& t, pos p); //Complejidad: O(1) constante
-void cambiarBanderita(tablero& t, jugadas& j, pos p, banderitas& b); //Complejidad: O(n)
-bool perdio(tablero& t, jugadas& j); //Complejidad: O(n)
-bool gano(tablero& t, jugadas& j);
-void jugarPlus(tablero& t, banderitas& b, pos p, jugadas& j);
-bool sugerirAutomatico121(tablero& t, banderitas& b, jugadas& j, pos& p);
+void cambiarBanderita(tablero& t, jugadas& j, pos p, banderitas& b); //Complejidad: O(|b|)
+bool perdio(tablero& t, jugadas& j); //Complejidad: O(|j|)
+bool gano(tablero& t, jugadas& j); //Complejidad: O(|j|^2 + |t|^2)
+void jugarPlus(tablero& t, banderitas& b, pos p, jugadas& j); //Complejidad: O(|t|*(|b|+|j|))
+bool sugerirAutomatico121(tablero& t, banderitas& b, jugadas& j, pos& p);//Complejidad: O(|t|^2(|b|+|j|))
 
