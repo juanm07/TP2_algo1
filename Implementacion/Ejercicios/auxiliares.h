0f403c8942680af6f5a692364bc6a42836c95384
juanm07 <63691303+juanm07@users.noreply.github.com>
2022-12-27 16:12:54 -0300

    Update and rename Ejercicios/auxiliares.h to Implementacion/Ejercicios/auxiliares.h

--- Implementacion/Ejercicios/auxiliares.h
+++ Implementacion/Ejercicios/auxiliares.h
@@ -1,9 +1,7 @@
 
-//
 // Este archivo contiene las declaraciones de las funciones auxiliares
-// que ustedes necesiten implementar. Tienen que coincidir con el código
-// de esas mismas funciones, que estará en auxiliares.cpp
-//
+// el código de esas mismas funciones, estará en auxiliares.cpp
+
 
 #pragma once   // esta línea es para que el archivo se incluya una sola vez
 
